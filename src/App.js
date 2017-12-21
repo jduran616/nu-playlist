6a5ceef8f9b04b422a3ad7a95344702b380886d0
jduran616 <jduran616@gmail.com>
2017-12-21 10:46:03 -0800

    changed app background and removed logo from files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 let defaultTextColor = '#FFF';
 let defaultStyle = {
