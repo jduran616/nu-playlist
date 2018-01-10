4999dc7747a6100216734c0ee0e34a234cf7710c
jduran616 <jduran005@student.butte.edu>
2018-01-10 14:54:28 -0800

    Filter changes hours and playlist counter

--- src/App.js
+++ src/App.js
@@ -149,6 +149,8 @@ class App extends Component {
     }, 1000);
   };
   render() {
+    let playlistsToRender = this.state.serverData.user ? this.state.serverData.user.playlists.filter(playlist =>
+      playlist.name.toLowerCase().includes(this.state.filterString.toLowerCase())) : []
     return (
       <div className="App">
         {this.state.serverData.user ?
@@ -157,15 +159,13 @@ class App extends Component {
               {this.state.serverData.user.name}s Playlist
             </h1>
 
-            <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
+            <PlaylistCounter playlists={playlistsToRender}/>
 
-            <HoursCounter playlists={this.state.serverData.user.playlists}/>
+            <HoursCounter playlists={playlistsToRender}/>
 
             <Filter onTextChange={text => this.setState({filterString: text})}/>
 
-            {this.state.serverData.user.playlists.filter(playlist =>
-              playlist.name.toLowerCase().includes(this.state.filterString.toLowerCase())
-            ).map(playlist =>
+            {playlistsToRender.map(playlist =>
               <Playlist playlist={playlist}/>
             )}
 
